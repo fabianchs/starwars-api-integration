2aa6f551c52d629d3257fce0a87669e93e4fef7a
fabianchs <fabichasola@hotmail.com>
2021-03-16 02:44:54 +0000

    archivos iniciales

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -8,9 +8,33 @@ export const Navbar = () => {
 				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
 			</Link>
 			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Coontext in action</button>
-				</Link>
+				<div className="dropdown">
+					<button
+						className="btn btn-secondary dropdown-toggle"
+						type="button"
+						id="dropdownMenuButton1"
+						data-bs-toggle="dropdown"
+						aria-expanded="false">
+						Favorites
+					</button>
+					<ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+						<li>
+							<a className="dropdown-item" href="#">
+								Action
+							</a>
+						</li>
+						<li>
+							<a className="dropdown-item" href="#">
+								Another action
+							</a>
+						</li>
+						<li>
+							<a className="dropdown-item" href="#">
+								Something else here
+							</a>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</nav>
 	);
