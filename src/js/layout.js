e88402c4452b4a13a7546bc6beeafca30b46c31c
fabianchs <fabichasola@hotmail.com>
2021-03-16 02:55:05 +0000

    avance

--- src/js/layout.js
+++ src/js/layout.js
@@ -5,10 +5,12 @@ import ScrollToTop from "./component/scrollToTop";
 import { Home } from "./views/home";
 import { Demo } from "./views/demo";
 import { Single } from "./views/single";
+
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { Cards } from "./component/cards";
 
 //create your first component
 const Layout = () => {
@@ -21,6 +23,7 @@ const Layout = () => {
 			<BrowserRouter basename={basename}>
 				<ScrollToTop>
 					<Navbar />
+					<Cards />
 					<Switch>
 						<Route exact path="/">
 							<Home />
