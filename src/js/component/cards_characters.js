25070d623b5c4f3f5eccc6bd9fa3f5642d4af70a
fabianchs <fabichasola@hotmail.com>
2021-03-18 02:30:43 +0000

    avance en cards

--- src/js/component/cards_characters.js
+++ src/js/component/cards_characters.js
@@ -2,5 +2,25 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 export function Cards() {
-	return <div>Hola</div>;
+	const w_card = {
+		width: "300px"
+	};
+	return (
+		<div>
+			<div className="card" style={w_card}>
+				<img
+					src="https://play-lh.googleusercontent.com/f0jCBnEIgln1wJ6V-LXFWMwJ-ZVklD02ueJWCUx8hlhspylOUpLLJh9rx_sMWDgML-0K"
+					className="card-img-top"
+					alt="..."
+				/>
+				<div className="card-body">
+					<h5 className="card-title">Card title</h5>
+					<p className="card-text">Some quick example text to build</p>
+					<a href="#" className="btn btn-primary">
+						Go somewhere
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 }
